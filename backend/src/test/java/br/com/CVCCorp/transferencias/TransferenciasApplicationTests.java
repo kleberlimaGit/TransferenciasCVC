9d2dc51d53cda2760c0b9bd21fbb1acc93d893b5
kleberlimagit <mov_kleber@hotmail.com>
2021-07-21 08:22:12 -0300

    first commit

---
+++ backend/src/test/java/br/com/CVCCorp/transferencias/TransferenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.CVCCorp.transferencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
